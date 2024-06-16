24bb344738a3f359dd383cdc0e08933b3742c8b0
N RAMESH KANNAN <43806145+kannan-ramesh@users.noreply.github.com>
2024-06-16 12:01:17 +0530

    level 2 commit

--- game/src/com/kannanrameshrk/connectfour/ConnectFour.java
+++ game/src/com/kannanrameshrk/connectfour/ConnectFour.java
@@ -3,100 +3,131 @@ package com.kannanrameshrk.connectfour;
 import java.util.Scanner;
 
 public class ConnectFour {
-	
-	
+
 	public static void main(String[] args) {
-		
-		Scanner input=new Scanner(System.in);
-		char[][] board=new char[6][7];
-		
+		Scanner input = new Scanner(System.in);
+		char[][] board = new char[6][7];
+
 		fillArray(board);
-		int turn=1;
-		boolean winner=false;
-		char player='R';
-		
-		while(winner == false && turn<=42) {
+		int turn = 1;
+		boolean winner = false;
+		char player = 'R';
+
+		while (winner == false && turn <= 42) {
 			boolean validPlay = false;
 			int play;
 			do {
 				display(board);
-				System.out.println("player "+player+" choose a column(0-6)");
-				play=input.nextInt();
-				validPlay=validate(play,board);
-				
-			}while(validPlay == false);
-			
-			//drop the tocken board
-			
-			for(int i=board.length-1;i>=0;i--) {
-				if(board[i][play]=='-') {
-					board[i][play]=player;
+				if (player == 'R') {
+                    System.out.println("Player " + player + " choose a column (0-6)");
+                    play = input.nextInt();
+                } else {
+                    play = getComputerMove(board);
+                    System.out.println("Computer (Player Y) chooses column " + play);
+                }
+				validPlay = validate(play, board);
+
+			} while (validPlay == false);
+
+			// drop the tocken board
+
+			for (int i = board.length - 1; i >= 0; i--) {
+				if (board[i][play] == '-') {
+					board[i][play] = player;
 					break;
 				}
 			}
-			
-			//detarmin winner
-			
-			winner=isWinner(player,board);
-			
-			//switch player
-			if(player=='R') {
-				player='Y';
-			}else {
-				player='R';
+
+			// detarmin winner
+
+			winner = isWinner(player, board);
+
+			// switch player
+			if (player == 'R') {
+				player = 'Y';
+			} else {
+				player = 'R';
 			}
 			turn++;
 		}
 		display(board);
-		
-		if(winner) {
-			if(player=='R') {
+
+		if (winner) {
+			if (player == 'R') {
 				System.out.println("Player Yellow is Winner..");
-			}else {
+			} else {
 				System.out.println("Player Red is Winner..");
 			}
-		}else {
+		} else {
 			System.out.println("Tie game..");
 		}
-		
+	}
+
+	private static int getComputerMove(char[][] board) {
+		for (int col = 0; col < board[0].length; col++) {
+            if (validate(col, board)) {
+                // Temporarily make the move
+                for (int row = board.length - 1; row >= 0; row--) {
+                    if (board[row][col] == '-') {
+                        board[row][col] = 'R';
+                        if (isWinner('R', board)) {
+                            board[row][col] = '-'; // Undo the move
+                            return col; // Block this move
+                        }
+                        board[row][col] = '-'; // Undo the move
+                        break;
+                    }
+                }
+            }
+        }
+        // If no immediate threat, choose the first available column
+        for (int col = 0; col < board[0].length; col++) {
+            if (validate(col, board)) {
+                return col;
+            }
+        }
+        return -1;
 	}
 
 	private static boolean isWinner(char player, char[][] board) {
-		//check for four across
-		
-		for(int i=0;i<board.length;i++) {
-			for(int j=0;j<board[0].length-3;j++) {
-				if(board[i][j]==player && board[i][j+1] == player && board[i][j+2]==player && board[i][j+3]==player) {
+		// check for four across
+
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length - 3; j++) {
+				if (board[i][j] == player && board[i][j + 1] == player && board[i][j + 2] == player
+						&& board[i][j + 3] == player) {
 					return true;
 				}
 			}
 		}
-		
-		//check four up and down
-		
-		for(int i=0;i<board.length-3;i++) {
-			for(int j=0;j<board[0].length;j++) {
-				if(board[i][j]==player && board[i+1][j]==player && board[i+2][j]==player && board[i+3][j]==player) {
+
+		// check four up and down
+
+		for (int i = 0; i < board.length - 3; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j] == player && board[i + 1][j] == player && board[i + 2][j] == player
+						&& board[i + 3][j] == player) {
 					return true;
 				}
 			}
 		}
-		
-		//check upward diagnol
-		
-		for(int i=3;i<board.length;i++) {
-			for(int j=0;j<board[0].length-3;j++) {
-				if(board[i][j]==player && board[i-1][j+1]==player && board[i-2][j+2]==player && board[i-3][j+3]==player) {
+
+		// check upward diagnol
+
+		for (int i = 3; i < board.length; i++) {
+			for (int j = 0; j < board[0].length - 3; j++) {
+				if (board[i][j] == player && board[i - 1][j + 1] == player && board[i - 2][j + 2] == player
+						&& board[i - 3][j + 3] == player) {
 					return true;
 				}
 			}
 		}
-		
-		//check downward diagonal
-		
-		for(int i=0;i<board.length-3;i++) {
-			for(int j=0;j<board[0].length-3;j++) {
-				if(board[i][j] == player && board[i+1][j+1]==player && board[i+2][j+2]==player && board[i+3][j+3]==player) {
+		// check downward diagonal
+
+		for (int i = 0; i < board.length - 3; i++) {
+			for (int j = 0; j < board[0].length - 3; j++) {
+				if (board[i][j] == player && board[i + 1][j + 1] == player && board[i + 2][j + 2] == player
+						&& board[i + 3][j + 3] == player) {
 					return true;
 				}
 			}
@@ -105,33 +136,31 @@ public class ConnectFour {
 	}
 
 	private static boolean validate(int n, char[][] board) {
-		if(n<0 && n>board[0].length) {
+		if (n < 0 && n > board[0].length) {
 			return false;
 		}
-		
-		if(board[0][n]!='-') {
+
+		if (board[0][n] != '-') {
 			return false;
 		}
 		return true;
 	}
 
 	private static void display(char[][] board) {
-		for(int i=0;i<board.length;i++) {
-			for(int j=0;j<board[0].length;j++) {
-				System.out.print(board[i][j]+",");
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				System.out.print(board[i][j] + ",");
 			}
 			System.out.println();
 		}
 	}
 
 	private static void fillArray(char[][] board) {
-		for(int i=0;i<board.length;i++) {
-			for(int j=0;j<board[0].length;j++) {
-				board[i][j]='-';
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				board[i][j] = '-';
 			}
 		}
-		
-	}
 
-	
+	}
 }
